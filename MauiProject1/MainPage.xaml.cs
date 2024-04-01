76d057b782ba7c6702f7db97c7613d322ee29e1c
AndroidT50 <westfit@yandex.ru>
2024-04-01 17:07:05 +0700

    Начало

--- MauiProject1/MainPage.xaml.cs
+++ MauiProject1/MainPage.xaml.cs
@@ -1,15 +1,20 @@
-﻿namespace MauiProject1
+﻿using System.Collections.ObjectModel;
+
+namespace MauiProject1
 {
     public partial class MainPage : ContentPage
     {
-        
+        public ObservableCollection <string> Items { get; set; } = new ObservableCollection<string>();
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-     
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
     }
 
 }
