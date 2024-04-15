c2a7cf6634931a774c048d481691aa5544c5a8c3
Vint k <vintkorp@outlook.com>
2024-04-15 21:49:14 +0700

    дата готова

--- MauiProject1/MainPage.xaml.cs
+++ MauiProject1/MainPage.xaml.cs
@@ -1,36 +1,44 @@
-﻿using Network;
+﻿
 using System.Collections.ObjectModel;
 using System.Windows.Input;
+
 
 namespace MauiProject1
 {
     public partial class MainPage : ContentPage
     {
-        public ObservableCollection <string> Items { get; set; } = new ObservableCollection<string>();
-        public ICommand DeleteTaskCommand { get; private set; }
+        public ObservableCollection <TaskItem> Items { get; set; } = new ObservableCollection<TaskItem>();
+       
+        public ICommand DeleteCommand { get; private set; }
+        
         public MainPage()
         {
             InitializeComponent();
             BindingContext = this;
+            TaskCollectionView.BindingContext = this;
+            DeleteCommand = new Command<TaskItem>(SwipeItem_Clicked);
+            // Создание объекта с данными
         }
-
+        public class TaskItem
+        {
+            public string?Task { get; set; }
+            public DateTime Date { get; set; }
+        }
         private void Button_Clicked(object sender, EventArgs e)
-        {
-            if (!string.IsNullOrEmpty(PleaseHolder.Text))
+        {    
+            string taskName = PleaseHolder.Text.Trim();
+            if (!string.IsNullOrEmpty(taskName))
             {
-                //Items.Add(PleaseHolder.Text);
-                Items.Insert(0, PleaseHolder.Text);
+                Items.Add(new TaskItem { Task = taskName, Date = DateTime.Now });
                 PleaseHolder.Text = string.Empty;
-            }
-
+            }   
         }
-
-        DeleteTaskCommand = new Command <string>(DeleteTask);
-        private void DeleteTask(string task)
+        private void SwipeItem_Clicked(TaskItem task)
         {
-            //Items.Clear();
             Items.Remove(task);
         }
+
+       
     }
 
 }
